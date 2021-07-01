7d5d9940a94d478e34fc5964064fc8c195194d8f
Yash <yashpatel26@gmail.com>
2021-07-01 18:55:42 -0500

    finished vhost-creation script

--- python_scripts/vhost-creator.py
+++ python_scripts/vhost-creator.py
@@ -1,29 +1,42 @@
 #!/usr/bin/python3
-import os
-import sys
-import getopt
 from dotenv import load_dotenv
+import getopt
+import sys
+import os
 
+#  loading dot env file for environment vars
 load_dotenv()
+#  getting required vars from .env
 VHOST_DIR_PATH = os.getenv('VHOST_DIR_PATH')
 
 # name will start from 5{domain}.conf
 # create vhost file
 
 
-def createVhost(domainName):
-    fileName = VHOST_DIR_PATH + \
-        "5" + domainName + ".conf"
+def getFullDomainName(subDomain):
+    return subDomain + ".trypypy.com"
+
+
+def getFileName(subDomain, fullPath=False):
+    if fullPath:
+        return VHOST_DIR_PATH + "5" + subDomain + ".conf"
+    return "5" + subDomain + ".conf"
+
+
+def createVhost(subDomain):
+    # creating string for vhost file
     writeString = f"""<VirtualHost *:80>
-	ServerName {domainName + ".trypypy.com"}
+	ServerName {getFullDomainName(subDomain)}
 	DocumentRoot /var/www/trypypy/root_domain/
 	ErrorLog /var/www/trypypy/root_domain/logs/sub-error.log
 	CustomLog /var/www/trypypy/root_domain/logs/sub-access.log combined
     RewriteEngine on
-    RewriteCond %{{SERVER_NAME}} ={domainName + ".trypypy.com"}
+    RewriteCond %{{SERVER_NAME}} ={getFullDomainName(subDomain)}
     RewriteRule ^ https://%{{SERVER_NAME}}%{{REQUEST_URI}} [END,NE,R=permanent]
 </VirtualHost>"""
-    f = open(fileName, "w")
+
+    # creating and writing to vhost file
+    f = open(getFileName(subDomain, fullPath=True), "w")
     f.write(writeString)
     f.close()
 
@@ -35,16 +48,43 @@ if __name__ == "__main__":
             print('Usage: ./vhost-creator.py -d <domainName>')
         for opt, arg in opts:
             if opt == '-d':
-                createVhost(arg)
+                print('Creating vhost file')
+                subDomain = arg
+                createVhost(subDomain)
+                print('vhost file created: ' +
+                      getFileName(subDomain, fullPath=True))
+                print('full domain name: ' + getFullDomainName(subDomain))
+
+                # shell command starts
+
+                # enabling apache 2 site
+                ensiteStatus = os.system(f'a2ensite {getFileName(subDomain)}')
+                if ensiteStatus == 0:
+                    print('apache2 site enabled')
+
+                    # getting ssl certificate
+                    certificateStatus = os.system(
+                        f'certbot --apache -n -d {getFullDomainName(subDomain)}')
+                    if certificateStatus == 0:
+                        print('SSL certificate issued')
+
+                        # reloading apache2
+                        reloadStatus = os.system('systemctl reload apache2')
+                        if reloadStatus == 0:
+                            print('Server is reloaded')
+                        else:
+                            print('There was error reloading apache2 server')
+                            sys.exit()
+                    else:
+                        print('There was error issuing SSL certificate')
+                        sys.exit()
+                else:
+                    print('there was problem wit a2ensite command')
+                    sys.exit()
+
+                # shell command ends
+
                 sys.exit()
     except getopt.GetoptError as err:
         print('Index error: ', err)
         print('please try again')
-
-# enable site
-    # a2ensite filename
-
-# test vhost file
-    # should be done along with creation of file
-# generate ssl
-# add https redirect rule inside vhost
