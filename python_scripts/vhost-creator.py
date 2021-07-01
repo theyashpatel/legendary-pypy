9b9cff3ac6e27767d5284a808eb17a8ad2d86238
Yash <yashpatel26@gmail.com>
2021-07-01 18:08:27 -0500

    https rewrite rule added

--- python_scripts/vhost-creator.py
+++ python_scripts/vhost-creator.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
+import os
 import sys
 import getopt
+from dotenv import load_dotenv
+
+load_dotenv()
+VHOST_DIR_PATH = os.getenv('VHOST_DIR_PATH')
+
 # name will start from 5{domain}.conf
 # create vhost file
 
 
 def createVhost(domainName):
-    fileName = "/etc/apache2/sites-available/" + \
+    fileName = VHOST_DIR_PATH + \
         "5" + domainName + ".conf"
     writeString = f"""<VirtualHost *:80>
 	ServerName {domainName + ".trypypy.com"}
 	DocumentRoot /var/www/trypypy/root_domain/
 	ErrorLog /var/www/trypypy/root_domain/logs/sub-error.log
 	CustomLog /var/www/trypypy/root_domain/logs/sub-access.log combined
+    RewriteEngine on
+    RewriteCond %{{SERVER_NAME}} ={domainName + ".trypypy.com"}
+    RewriteRule ^ https://%{{SERVER_NAME}}%{{REQUEST_URI}} [END,NE,R=permanent]
 </VirtualHost>"""
     f = open(fileName, "w")
     f.write(writeString)
@@ -22,6 +31,8 @@ def createVhost(domainName):
 if __name__ == "__main__":
     try:
         opts, args = getopt.getopt(sys.argv[1:], "d:")
+        if not opts:
+            print('Usage: ./vhost-creator.py -d <domainName>')
         for opt, arg in opts:
             if opt == '-d':
                 createVhost(arg)
