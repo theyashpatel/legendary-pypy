86035d701dc31dfc458e1746b0870ed289e6106e
Yash <yashpatel26@gmail.com>
2021-07-02 22:17:49 -0500

    changed script path

--- backend/api/controllers/subdomains.js
+++ backend/api/controllers/subdomains.js
@@ -2,9 +2,11 @@ const SubDomain = require('../models/subdomains')
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
+const python_scripts_path = '/stuff/legendary-pypy/python-scripts/'
+
 async function createSDScript(sd) {
 try {
-    const { stdout, stderr } = await exec(`vhost-creator - c ${sd}`);
+    const { stdout, stderr } = await exec(`${python_scripts_path}vhost-creator.py - c ${sd}`);
 }catch (err){
     console.error(err);
 };
@@ -12,7 +14,7 @@ try {
 
 async function deleteSDScript(sd) {
 try {
-    const { stdout, stderr } = await exec(`vhost-creator - d ${sd}`);
+    const { stdout, stderr } = await exec(`${python_scripts_path}vhost-creator.py - d ${sd}`);
 }catch (err){
     console.error(err);
 };
